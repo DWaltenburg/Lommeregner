856ce5c01adf4f9781f64b85a0f02fa886ccde02
David Waltenburg <david@waltenburg.dk>
2023-08-01 13:39:51 +0200

    old version fixed typo

--- main.py
+++ main.py
@@ -12,34 +12,18 @@ def add(x,y):
 def subtract(x,y):
     return x-y
 
-def power(x,y):
-    return pow(x,y)
-
-def squareRoot(x):
-    return math.sqrt(x)
-
-def modulus(x,y):
-    return x%y
-
-operator = int(input("Vælg en funktion: \n1 - plus\n2 - minus\n3 - gange\n4 - divider\n5 - power\n6 - square root\n7 - modulus\n"))
+operator = int(input("Vælg en funktion: \n1 - plus\n2 - minus\n3 - gange\n4 - divider\n"))
 tal1 = float(input("Vælg første tal: "))
 tal2 = float(input("Vælg andet tal: "))
 
 print("Resultat:")
-match operator:
-    case 1:
-        print(add(tal1,tal2))
-    case 2:
-        print(subtract(tal1,tal2))
-    case 3:
-        print(multiply(tal1,tal2))
-    case 4:
-        print(divide(tal1,tal2))
-    case 5:
-        print(power(tal1, tal2))
-    case 6:
-        print(squareRoot(tal1))
-    case 7:
-        print(modulus(tal1,tal2))
-    case _:
-        print("Ugyldig valg")
+if(operator == 1):
+    print(add(tal1,tal2))
+elif(operator == 2):
+    print(subtract(tal1,tal2))
+elif(operator == 3):
+    print(multiply(tal1,tal2))
+elif(operator == 4):
+    print(divide(tal1,tal2))
+else:
+    print("Ugyldig valg")
